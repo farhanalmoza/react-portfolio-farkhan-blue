60aa88227e2cbd8df75b340ae2e8a1fc9e28ba7c
Farhan <oyisamfarhan@gmail.com>
2022-07-15 15:35:53 +0700

    finish

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react'
+import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+import emailjs from 'emailjs-com';
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_0wwra4y', 'template_scwek5a', form.current, 'WIY-wDEQe46DmoLkM')
+
+    e.target.reset();
+  };
+
+  return (
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>farkhanjayadi@gmail.com</h5>
+            <a href="mailto:farkhanjayadi@gmail.com" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>WhatsApp</h4>
+            <h5>+6281395161765</h5>
+            <a href="https://wa.me/6281395161765" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required />
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
